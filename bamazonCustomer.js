e9b1e0c229a09fc7535269207c0b866cd5a16062
ctslone <ctslone27@gmail.com>
2019-11-22 18:25:18 -0500

    displaying raw results using console table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,4 +17,13 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     // then, do stuff
+    showAll();
 });
+
+function showAll() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    console.table(results)
+    connection.end()
+})
+}
