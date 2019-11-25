c9896db938936fd987f0621f2a3f30353aa77ef9
ctslone <ctslone27@gmail.com>
2019-11-25 18:42:13 -0500

    adding functionality with switch for view all products. prompts user for another command when a command finsihes.

--- bamazonManager.js
+++ bamazonManager.js
@@ -4,27 +4,63 @@ var table = require("console.table")
 
 // create the connection for DB
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  port: 3306,
+    port: 3306,
 
-  user: "root",
+    user: "root",
 
-  password: "Gibsonlp1!",
-  database: "bamazon_DB"
+    password: "Gibsonlp1!",
+    database: "bamazon_DB"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
-  if (err) throw err;
-  showAll();
+    if (err) throw err;
+    prompt();
 });
+
+function prompt () {
+    inquirer.prompt([
+        {
+            type: "checkbox",
+            name: "managerAction",
+            message: "Welcome! What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        },
+      ])
+        .then(function (chosenAction) {
+            var action = chosenAction.managerAction.toString();
+            switch (action) {
+                case 'View Products for Sale':
+                showAll() 
+            
+                break;
+                case 'View Low Inventory':
+                    
+            
+                break;
+                case 'Add to Inventory':
+                    
+            
+                break;
+                case 'Add New Product':
+            
+                break;
+            }
+            connection.end()
+        })
+}
+
+
+
 
 // listing all items in the DB
 function showAll() {
-  connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, results) {
-    if (err) throw err;
-    console.table(results);
-    // promptUser();
-  })
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        prompt()
+    })
 }
+
