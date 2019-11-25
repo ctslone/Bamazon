0dae6973bf8246c7e6994a6933be2f53812f5c6b
ctslone <ctslone27@gmail.com>
2019-11-25 16:10:07 -0500

    finsihed logic for purchasing. db updates with new stock, alerts user if insufficient quantity, and alerts user of their total purchase price.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,9 +17,7 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
-  // then, do stuff
   showAll();
-  // promptUser();
   // connection.end()
 });
 
@@ -55,27 +53,55 @@ function promptUser() {
         }
       ])
         .then(function (responseQuantity) {
-          // if (response) {
-          //   console.log(response)
-          // }
           userQuantity = responseQuantity.howMany
+          
           function checkStock() {
             connection.query("SELECT * FROM products", function (err, results) {
-              //     if (err) throw err;
-              //     console.log("inside")
-              //     console.log(results)
-              for (var i = 0; i < results.length; i++) {
-                // console.log(results[3].item_id)
-                // console.log(response.howMany)
-                // console.log("This is the user item ID " + userItemID)
-                console.log("The item ID is " + results[i].item_id + " and the quantity is " + results[i].stock_quantity)
-                // if (results[i].item_id === userItemID) {
-                //   console.log("Stock quantity is: " + results[i].stock_quanity)
-                // }
-                if (results[i].item_id == userItemID) {
-                  console.log("we have that")
-                }
+              if (err) throw err;
+              console.log("inside")
+              var userItemIndex = (userItemID - 1)
+
+              if (results[userItemIndex].stock_quantity >= userQuantity) {
+                console.log("Thanks for your order!");
+                
+
+                updateStock = results[userItemIndex].stock_quantity -= userQuantity;
+
+                console.log("The total cost of your order is $" + (updateStock * results[userItemIndex].price));
+
+                connection.query(
+                  "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: updateStock
+                    },
+                    {
+                      item_id: userItemID
+                    }
+                  ],
+                  function (err, res) {
+                    if (err) throw err;
+                  })
+                
               }
+              else {
+                console.log("There is not enough to fulfill your order! Please edit your order to be less.")
+                promptUser()
+              }
+              // for (var i = 0; i < results.length; i++) {
+              // console.log(results[3].item_id)
+              // console.log(response.howMany)
+              // console.log("This is the user item ID " + userItemID)
+              // console.log("The item ID is " + results[i].item_id + " and the quantity is " + results[i].stock_quantity)
+              // if (results[i].item_id === userItemID) {
+              //   console.log("Stock quantity is: " + results[i].stock_quanity)
+              // }
+
+              // if (results[i].item_id == userItemID) {
+              //   console.log("we have that")
+              //   console.log(results[userItemIndex].stock_quantity)
+              // }
+              // }
               connection.end()
             })
           }
