18899a67f4046dd47442e54d105fb48005d3f3ae
ctslone <ctslone27@gmail.com>
2019-11-25 13:05:29 -0500

    adding basic logic to match the user ID with the item ID from MYSQL.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,22 +33,55 @@ function showAll() {
 }
 
 function promptUser() {
+
   inquirer.prompt([
     {
       type: "input",
       name: "userID",
       message: "Enter the ID of the item you would like to buy.",
     },
-    {
-      type: "input",
-      name: "howMany",
-      message: "How many do you want to buy?",
-    }
   ])
-    .then(function (response) {
-      if (response) {
-        console.log(response)
-        connection.end()
-      }
+    .then(function (responseID) {
+      // if (response) {
+      //   console.log(response)
+      //   connection.end()
+      // }
+      var userItemID = responseID.userID
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "howMany",
+          message: "How many do you want to buy?",
+        }
+      ])
+        .then(function (responseQuantity) {
+          // if (response) {
+          //   console.log(response)
+          // }
+          userQuantity = responseQuantity.howMany
+          function checkStock() {
+            connection.query("SELECT * FROM products", function (err, results) {
+              //     if (err) throw err;
+              //     console.log("inside")
+              //     console.log(results)
+              for (var i = 0; i < results.length; i++) {
+                // console.log(results[3].item_id)
+                // console.log(response.howMany)
+                // console.log("This is the user item ID " + userItemID)
+                console.log("The item ID is " + results[i].item_id + " and the quantity is " + results[i].stock_quantity)
+                // if (results[i].item_id === userItemID) {
+                //   console.log("Stock quantity is: " + results[i].stock_quanity)
+                // }
+                if (results[i].item_id == userItemID) {
+                  console.log("we have that")
+                }
+              }
+              connection.end()
+            })
+          }
+          checkStock()
+        })
     })
 }
+
+
