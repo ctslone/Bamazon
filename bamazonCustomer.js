ed48269fdf1182c98f68334c0d27acf2eef2dea9
ctslone <ctslone27@gmail.com>
2019-11-30 14:00:47 -0500

    adding input validations(numbers only positive) for customer file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,60 +47,79 @@ function promptUser() {
       message: "Enter the ID of the item you would like to buy.",
     },
   ])
-  // asking user how many items they want
+    // asking user how many items they want
     .then(function (responseID) {
       // assigning the response to a shorter variable
       var userItemID = responseID.userID
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "howMany",
-          message: "How many do you want to buy?",
-        }
-      ])
-        .then(function (responseQuantity) {
-          // assigning the response to a shorter variable
-          userQuantity = responseQuantity.howMany
-          // checks the stock of the item requested and determines if a sale can be made
-          function checkStock() {
-            connection.query("SELECT * FROM products", function (err, results) {
-              if (err) throw err;
-              // setting a variable to use in the for loop, based on the users item slection. The index is one less than the actual item number
-              var userItemIndex = (userItemID - 1)
-              // if statement for if there is enough stock to fulfill the order
-              if (results[userItemIndex].stock_quantity >= userQuantity) {
-                console.log("Thanks for your order!");
-                // creating an updated stock to place in the DB
-                var updateStock = results[userItemIndex].stock_quantity -= userQuantity;
-
-                console.log("The total cost of your order is $" + (updateStock * results[userItemIndex].price));
-                // updating the DB based on the order
-                connection.query(
-                  "UPDATE products SET ? WHERE ?",
-                  [
-                    {
-                      stock_quantity: updateStock
-                    },
-                    {
-                      item_id: userItemID
-                    }
-                  ],
-                  function (err, res) {
-                    if (err) throw err;
-                  }
-                )
-              }
-              else {
-                console.log("There is not enough to fulfill your order! Please edit your order to be less.")
-                promptUser()
-              }
-              // ending the connection after the purchase
-              connection.end()
-            })
+      // testing for user input being positive numbers only
+      var onlyNumbers = /^[1-9]\d*$/.test(userItemID)
+      if (onlyNumbers) {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "howMany",
+            message: "How many do you want to buy?",
           }
-          // invoke checkstock function
-          checkStock()
-        })
+        ])
+          .then(function (responseQuantity) {
+            // assigning the response to a shorter variable
+            userQuantity = responseQuantity.howMany
+            var onlyNumbersQuant = /^[1-9]\d*$/.test(userQuantity)
+            console.log(onlyNumbersQuant)
+            // checks the stock of the item requested and determines if a sale can be made
+            if (onlyNumbersQuant) {
+              function checkStock() {
+                connection.query("SELECT * FROM products", function (err, results) {
+                  if (err) throw err;
+                  // setting a variable to use in the for loop, based on the users item slection. The index is one less than the actual item number
+                  var userItemIndex = (userItemID - 1)
+                  // if statement for if there is enough stock to fulfill the order
+                  if (results[userItemIndex].stock_quantity >= userQuantity) {
+                    console.log("Thanks for your order!");
+                    // creating an updated stock to place in the DB
+                    var updateStock = results[userItemIndex].stock_quantity -= userQuantity;
+
+                    console.log("The total cost of your order is $" + (updateStock * results[userItemIndex].price));
+                    // updating the DB based on the order
+                    connection.query(
+                      "UPDATE products SET ? WHERE ?",
+                      [
+                        {
+                          stock_quantity: updateStock
+                        },
+                        {
+                          item_id: userItemID
+                        }
+                      ],
+                      function (err, res) {
+                        if (err) throw err;
+                      }
+                    )
+                  }
+                  else {
+                    console.log("There is not enough to fulfill your order! Please edit your order to be less.")
+                    promptUser()
+                  }
+                  // ending the connection after the purchase
+                  connection.end()
+                })
+              }
+              // invoke checkstock function
+              checkStock()
+            }
+            // if user doesnt enter a positive number
+            else {
+              console.log("Please enter the ID as a positive number!")
+              promptUser()
+            }
+          })
+      }
+      // if user doesnt enter positive number
+      else {
+        console.log("Please enter the ID as a positive number!")
+        promptUser()
+      }
+
     })
 }
 
